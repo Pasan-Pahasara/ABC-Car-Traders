a7e3b94cd146dde8f4d097071bc3f42729cb7e55
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2024-08-19 11:17:49 +0530

    feat: implemented crud in customer

--- ABC-Car-Traders/AdminController/ManageCustomer.Designer.cs
+++ ABC-Car-Traders/AdminController/ManageCustomer.Designer.cs
@@ -29,21 +29,22 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtCustomerContact = new System.Windows.Forms.TextBox();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerAddress = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtCustomerNIC = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnCustomerUpdate = new System.Windows.Forms.Button();
+            this.btnCustomerDelete = new System.Windows.Forms.Button();
             this.dgvCustomer = new System.Windows.Forms.DataGridView();
+            this.btnCustomerAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,33 +58,33 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Admin / Manage Customer";
             // 
-            // textBox1
+            // txtCustomerContact
             // 
-            this.textBox1.Location = new System.Drawing.Point(107, 234);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(371, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtCustomerContact.Location = new System.Drawing.Point(107, 234);
+            this.txtCustomerContact.Name = "txtCustomerContact";
+            this.txtCustomerContact.Size = new System.Drawing.Size(371, 22);
+            this.txtCustomerContact.TabIndex = 1;
             // 
-            // textBox2
+            // txtCustomerId
             // 
-            this.textBox2.Location = new System.Drawing.Point(685, 116);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(211, 22);
-            this.textBox2.TabIndex = 2;
+            this.txtCustomerId.Location = new System.Drawing.Point(685, 116);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(211, 22);
+            this.txtCustomerId.TabIndex = 2;
             // 
-            // textBox3
+            // txtCustomerName
             // 
-            this.textBox3.Location = new System.Drawing.Point(107, 183);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(371, 22);
-            this.textBox3.TabIndex = 3;
+            this.txtCustomerName.Location = new System.Drawing.Point(107, 183);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(371, 22);
+            this.txtCustomerName.TabIndex = 3;
             // 
-            // textBox4
+            // txtCustomerAddress
             // 
-            this.textBox4.Location = new System.Drawing.Point(607, 183);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(371, 22);
-            this.textBox4.TabIndex = 4;
+            this.txtCustomerAddress.Location = new System.Drawing.Point(607, 183);
+            this.txtCustomerAddress.Name = "txtCustomerAddress";
+            this.txtCustomerAddress.Size = new System.Drawing.Size(371, 22);
+            this.txtCustomerAddress.TabIndex = 4;
             // 
             // label2
             // 
@@ -135,12 +136,12 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "NIC";
             // 
-            // textBox5
+            // txtCustomerNIC
             // 
-            this.textBox5.Location = new System.Drawing.Point(607, 234);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(371, 22);
-            this.textBox5.TabIndex = 10;
+            this.txtCustomerNIC.Location = new System.Drawing.Point(607, 234);
+            this.txtCustomerNIC.Name = "txtCustomerNIC";
+            this.txtCustomerNIC.Size = new System.Drawing.Size(371, 22);
+            this.txtCustomerNIC.TabIndex = 10;
             // 
             // button1
             // 
@@ -160,26 +161,29 @@
             this.button3.Text = "Search";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnCustomerUpdate
             // 
-            this.button2.Location = new System.Drawing.Point(723, 283);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 46);
-            this.button2.TabIndex = 14;
-            this.button2.Text = "Update";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCustomerUpdate.Location = new System.Drawing.Point(723, 283);
+            this.btnCustomerUpdate.Name = "btnCustomerUpdate";
+            this.btnCustomerUpdate.Size = new System.Drawing.Size(120, 46);
+            this.btnCustomerUpdate.TabIndex = 14;
+            this.btnCustomerUpdate.Text = "Update Customer";
+            this.btnCustomerUpdate.UseVisualStyleBackColor = true;
+            this.btnCustomerUpdate.Click += new System.EventHandler(this.btnCustomerUpdate_Click);
             // 
-            // button4
+            // btnCustomerDelete
             // 
-            this.button4.Location = new System.Drawing.Point(858, 283);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(120, 46);
-            this.button4.TabIndex = 15;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnCustomerDelete.Location = new System.Drawing.Point(858, 283);
+            this.btnCustomerDelete.Name = "btnCustomerDelete";
+            this.btnCustomerDelete.Size = new System.Drawing.Size(120, 46);
+            this.btnCustomerDelete.TabIndex = 15;
+            this.btnCustomerDelete.Text = "Delete Customer";
+            this.btnCustomerDelete.UseVisualStyleBackColor = true;
+            this.btnCustomerDelete.Click += new System.EventHandler(this.btnCustomerDelete_Click);
             // 
             // dgvCustomer
             // 
+            this.dgvCustomer.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCustomer.Location = new System.Drawing.Point(40, 358);
             this.dgvCustomer.Name = "dgvCustomer";
@@ -187,27 +191,39 @@
             this.dgvCustomer.RowTemplate.Height = 24;
             this.dgvCustomer.Size = new System.Drawing.Size(938, 313);
             this.dgvCustomer.TabIndex = 16;
+            this.dgvCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCustomer_CellClick);
+            // 
+            // btnCustomerAdd
+            // 
+            this.btnCustomerAdd.Location = new System.Drawing.Point(585, 283);
+            this.btnCustomerAdd.Name = "btnCustomerAdd";
+            this.btnCustomerAdd.Size = new System.Drawing.Size(120, 46);
+            this.btnCustomerAdd.TabIndex = 17;
+            this.btnCustomerAdd.Text = "+ Add Customer";
+            this.btnCustomerAdd.UseVisualStyleBackColor = true;
+            this.btnCustomerAdd.Click += new System.EventHandler(this.btnCustomerAdd_Click);
             // 
             // ManageCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnCustomerAdd);
             this.Controls.Add(this.dgvCustomer);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCustomerDelete);
+            this.Controls.Add(this.btnCustomerUpdate);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.txtCustomerNIC);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCustomerAddress);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.txtCustomerContact);
             this.Controls.Add(this.label1);
             this.Name = "ManageCustomer";
             this.Size = new System.Drawing.Size(1041, 762);
@@ -221,20 +237,21 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtCustomerContact;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerAddress;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtCustomerNIC;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnCustomerUpdate;
+        private System.Windows.Forms.Button btnCustomerDelete;
         private System.Windows.Forms.DataGridView dgvCustomer;
+        private System.Windows.Forms.Button btnCustomerAdd;
     }
 }
--- ABC-Car-Traders/AdminController/ManageCustomer.cs
+++ ABC-Car-Traders/AdminController/ManageCustomer.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ABC_Car_Traders.AdminController
 {
@@ -56,6 +57,162 @@ namespace ABC_Car_Traders.AdminController
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void btnCustomerUpdate_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(Properties.Settings.Default.ABC_Car_TradersConnectionString))
+            {
+                // UPDATE QUERY
+                // Command Text with parameters                    
+                // string sqlCommandText = "INSERT INTO Book (BookId,ISBN,Title,Author,Genre,PublishedYear) VALUES (@BookId,@ISBN,@Title,@Author,@Genre,@PublishedYear)";
+                string sqlCommandText = "UPDATE Customer SET Name = @Name, Address = @Address, Contact = @Contact, NIC = @NIC WHERE ID = @ID";
+                // Set Parameters of the sql Command text
+                using (SqlCommand sqlCommand = new SqlCommand(sqlCommandText, dbConnection))
+                {
+                    // Set Parameters of the sql Command text
+                    sqlCommand.Parameters.Add(new SqlParameter("@ID", SqlDbType.VarChar));
+                    sqlCommand.Parameters["@ID"].Value = txtCustomerId.Text;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@Name", SqlDbType.VarChar));
+                    sqlCommand.Parameters["@Name"].Value = txtCustomerName.Text;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@Address", SqlDbType.VarChar));
+                    sqlCommand.Parameters["@Address"].Value = txtCustomerAddress.Text;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@Contact", SqlDbType.VarChar));
+                    sqlCommand.Parameters["@Contact"].Value = txtCustomerContact.Text;
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@NIC", SqlDbType.VarChar));
+                    sqlCommand.Parameters["@NIC"].Value = txtCustomerNIC.Text;
+
+                    try
+                    {
+                        // Open database connection
+                        dbConnection.Open();
+
+                        // Execute the command
+                        int result = sqlCommand.ExecuteNonQuery();
+
+                    }
+                    catch (SqlException sqlEx)
+                    {
+                        MessageBox.Show(sqlEx.Message);
+                    }
+                    finally
+                    {
+                        ManageCustomer_Load(sender, e);
+                        dbConnection.Close();
+                    }
+
+                }
+            }
+        }
+
+        private void dgvCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Ensure the user clicks on a cell that is not a header
+            if (e.RowIndex >= 0)
+            {
+                // Get the current row
+                DataGridViewRow row = dgvCustomer.Rows[e.RowIndex];
+
+                // Assign the cell values to the text fields
+                txtCustomerId.Text = row.Cells[0].Value.ToString();
+                txtCustomerName.Text = row.Cells[1].Value.ToString();
+                txtCustomerAddress.Text = row.Cells[2].Value.ToString();
+                txtCustomerContact.Text = row.Cells[3].Value.ToString();
+                txtCustomerNIC.Text = row.Cells[3].Value.ToString();
+            }
+        }
+
+        private void btnCustomerDelete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(Properties.Settings.Default.ABC_Car_TradersConnectionString))
+            {
+                string sqlCommandText = "DELETE FROM Customer WHERE ID=@ID";
+                using (SqlCommand sqlCommand = new SqlCommand(sqlCommandText, dbConnection))
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter("@ID", SqlDbType.VarChar));
+                    sqlCommand.Parameters["@ID"].Value = txtCustomerId.Text;
+
+                    try
+                    {
+                        // Open database connection
+                        dbConnection.Open();
+
+                        // Execute the command
+                        int result = sqlCommand.ExecuteNonQuery();
+
+                    }
+                    catch (SqlException sqlEx)
+                    {
+                        MessageBox.Show(sqlEx.Message);
+                    }
+                    finally
+                    {
+                        ManageCustomer_Load(sender, e);
+                        dbConnection.Close();
+                    }
+                }
+            }
+
+        }
+
+        private void btnCustomerAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create the connection object
+                using (SqlConnection dbConnection = new SqlConnection(Properties.Settings.Default.ABC_Car_TradersConnectionString))
+                {
+                    // Command Text with parameters                    
+                    string sqlCommandText = "INSERT INTO Customer (ID,Name,Address,Contact,NIC) VALUES (@ID,@Name,@Address,@Contact,@NIC)";
+
+                    using (SqlCommand sqlCommand = new SqlCommand(sqlCommandText, dbConnection))
+                    {
+                        // Set Parameters of the sql Command text
+                        sqlCommand.Parameters.Add(new SqlParameter("@ID", SqlDbType.VarChar));
+                        sqlCommand.Parameters["@ID"].Value = txtCustomerId.Text;
+
+                        sqlCommand.Parameters.Add(new SqlParameter("@Name", SqlDbType.VarChar));
+                        sqlCommand.Parameters["@Name"].Value = txtCustomerName.Text;
+
+                        sqlCommand.Parameters.Add(new SqlParameter("@Address", SqlDbType.VarChar));
+                        sqlCommand.Parameters["@Address"].Value = txtCustomerAddress.Text;
+
+                        sqlCommand.Parameters.Add(new SqlParameter("@Contact", SqlDbType.VarChar));
+                        sqlCommand.Parameters["@Contact"].Value = txtCustomerContact.Text;
+
+                        sqlCommand.Parameters.Add(new SqlParameter("@NIC", SqlDbType.VarChar));
+                        sqlCommand.Parameters["@NIC"].Value = txtCustomerNIC.Text;
+
+                        try
+                        {
+                            // Open database connection
+                            dbConnection.Open();
+
+                            // Execute the command
+                            int result = sqlCommand.ExecuteNonQuery();
+
+                        }
+                        catch (SqlException sqlEx)
+                        {
+                            MessageBox.Show(sqlEx.Message);
+                        }
+                        finally
+                        {
+                            ManageCustomer_Load(sender, e);
+                            dbConnection.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
